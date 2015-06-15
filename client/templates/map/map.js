5daf1429c6ece5c15656167aa5cfbe9d22f89ce0
Martin Pham <mdpham@uwaterloo.ca>
2015-06-15 17:41:36 -0400

    added sidebar nav, map

---
+++ client/templates/map/map.js
@@ -0,0 +1,19 @@
+Meteor.startup(function(){
+	var canvas = $("#map-canvas");
+	canvas.height(canvas.width()/2);
+	// var dims = [canvas.width(), canvas.height()];
+	// console.log(dims);
+
+	// create a map in the "map" div, set the view to a given place and zoom
+	var map = L.map('map-canvas').setView([51.505, -0.09], 13);
+
+	// add an OpenStreetMap tile layer
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+	// add a marker in the given location, attach some popup content to it and open the popup
+	L.marker([51.5, -0.09]).addTo(map)
+	    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+	    .openPopup();
+})
