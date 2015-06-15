35793f06d0bc1f594839d006f8ef7c4f5051a5f8
Martin Pham <mdpham@uwaterloo.ca>
2015-06-15 18:11:23 -0400

    added leaflet logo leaf icons

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -1,19 +1,42 @@
 Meteor.startup(function(){
 	var canvas = $("#map-canvas");
 	canvas.height(canvas.width()/2);
-	// var dims = [canvas.width(), canvas.height()];
-	// console.log(dims);
 
-	// create a map in the "map" div, set the view to a given place and zoom
-	var map = L.map('map-canvas').setView([51.505, -0.09], 13);
+	//Map
+	var map = L.map('map-canvas', {
+		zoomControl: false
+	}).setView([51.505, -0.09], 13);
 
-	// add an OpenStreetMap tile layer
+	//OpenStreetMap tile layer
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
+	//Zoom control
+	L.control.zoom({
+		position: "topright"
+	}).addTo(map)
+
+	//Marker icons
+	var LeafIcon = L.Icon.extend({
+		options: {
+	        shadowUrl: 'map/leaf-shadow.png',
+	        iconSize:     [38, 95],
+	        shadowSize:   [50, 64],
+	        iconAnchor:   [22, 94],
+	        shadowAnchor: [4, 62],
+	        popupAnchor:  [-3, -76]
+	    }
+	})
+	var greenIcon = new LeafIcon({iconUrl: "map/leaf-green.png"});
 	// add a marker in the given location, attach some popup content to it and open the popup
-	L.marker([51.5, -0.09]).addTo(map)
-	    .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+	L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map)
+	    .bindPopup('Custom leaf icon')
 	    .openPopup();
+
+	L.easyButton("fa-bars", 
+	              function () {$("#wrapper").toggleClass("toggled");},
+	             "Timeline",
+	             map
+	            )
 })
