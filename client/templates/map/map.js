4e48f18e20c9bf7e298e55427434b9da550266f1
Martin Pham <mdpham@uwaterloo.ca>
2015-06-18 01:22:33 -0400

    prevent panning to off the edge of the world

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -1,4 +1,55 @@
-Meteor.startup(function(){
+// Meteor.startup(function(){
+// 	var canvas = $("#map-canvas");
+// 	canvas.height(canvas.width()/2);
+
+// 	//Map
+// 	map = L.map('map-canvas', {
+// 		minZoom: 2,
+// 		maxZoom: 10,
+// 		zoomControl: false,
+// 		maxBounds: [[0,180],[-180,180]]
+// 	})
+// 	map.setView([43.4667, -80.516], 2)
+
+// 	//OpenStreetMap tile layer
+// 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+// 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+// 	    continuousWorld: false,
+// 	    minZoom: 2,
+// 	    maxZoom: 10
+// 	}).addTo(map);
+
+// 	//Zoom control
+// 	L.control.zoom({
+// 		position: "topright"
+// 	}).addTo(map)
+
+// 	//Marker icons
+// 	var LeafIcon = L.Icon.extend({
+// 		options: {
+// 	        shadowUrl: 'leaflet/leaf-shadow.png',
+// 	        iconSize:     [38, 95],
+// 	        shadowSize:   [50, 64],
+// 	        iconAnchor:   [22, 94],
+// 	        shadowAnchor: [4, 62],
+// 	        popupAnchor:  [-3, -76]
+// 	    }
+// 	})
+// 	var greenIcon = new LeafIcon({iconUrl: "leaflet/leaf-green.png"});
+// 	// add a marker in the given location, attach some popup content to it and open the popup
+// 	// L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map)
+// 	L.marker([43.4667, -80.516],{icon: greenIcon}).addTo(map)
+// 	    .bindPopup('Custom leaf icon')
+// 	    .openPopup();
+
+// 	L.easyButton("fa-share-alt fa-flip-horizontal", 
+// 	              function () {$(".ui.sidebar").sidebar("setting","transition","scale down").sidebar("toggle")},
+// 	             "Timeline",
+// 	             map
+// 	            )
+// })
+
+Template.map.rendered = function(){
 	var canvas = $("#map-canvas");
 	canvas.height(canvas.width()/2);
 
@@ -6,13 +57,19 @@ Meteor.startup(function(){
 	map = L.map('map-canvas', {
 		minZoom: 2,
 		maxZoom: 10,
-		zoomControl: false
+		zoomControl: false,
+		// maxBounds: [[0,180],[0,0]]
+		maxBounds: L.latLngBounds(L.latLng(-90,-180),L.latLng(90,180))
 	})
 	map.setView([43.4667, -80.516], 2)
 
+
 	//OpenStreetMap tile layer
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+	    continuousWorld: false,
+	    minZoom: 2,
+	    maxZoom: 10
 	}).addTo(map);
 
 	//Zoom control
@@ -43,4 +100,5 @@ Meteor.startup(function(){
 	             "Timeline",
 	             map
 	            )
-})
+
+}
