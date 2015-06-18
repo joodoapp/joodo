330645db574dfa9e2d28b0bcdd77db435c77870e
Martin Pham <mdpham@uwaterloo.ca>
2015-06-18 18:46:03 -0400

    wip

--- client/templates/sidebar/sidebar.js
+++ client/templates/sidebar/sidebar.js
@@ -21,10 +21,8 @@ sidebar = {
 	},
 	addStoryModal: function() {
 		//Init both add-story-modal and add-story-date-modal
-		$(".basic.modal.add-story-modal")
+		$(".modal.add-story-modal")
 			.modal("show");
-		$(".add-story-modal.start-date")
-			.pickadate();
 	}
 };
 
@@ -36,3 +34,30 @@ Template.sidebar.events({
 		sidebar.addStoryModal();
 	}
 });
+
+Template.addStory.rendered = function(e){
+	//Initalize addStory, startDate endDate modals
+	// $(".add-story-start-modal, .add-story-end-modal, .add-story-modal").modal({
+	// 	closable: false
+	// });
+
+	// $(".add-story-start-btn").on("click", function(e){
+	// 	console.log(e);
+	
+		// $(".add-story-start-modal").modal("show");
+		$(".add-story.start-date").pickadate({
+			container: "#start-date-picker"
+		});
+		// $(".add-story.start-date").pickadate("open");
+	// })
+	$(".add-story-end-btn").on("click", function(e){
+		console.log(e);
+		// $(".add-story-modal.end-date").pickadate({
+			// container: "#end-date-picker"
+		// });
+		$(".add-story-modal").modal("hide")
+		$(".add-story-end-modal").modal("show")
+		
+		// $(".add-story-modal.end-date").pickadate();
+	})
+}
